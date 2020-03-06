386b6e420ad9bb0767fc0671cff22ec30a8227c3
Yaroslav <GUSbYa@gmail.com>
2020-03-06 15:02:55 +0100

    added console publisher

--- publisher/src/main/java/lab/experimental/publisher/PublisherApplication.java
+++ publisher/src/main/java/lab/experimental/publisher/PublisherApplication.java
@@ -1,13 +1,59 @@
 package lab.experimental.publisher;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class PublisherApplication {
+public class PublisherApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PublisherApplication.class, args);
+		log.info("Publisher is up and running ...");
 	}
 
+	private static Queue<String> broker = new LinkedList<>();
+	private static StringBuilder event = new StringBuilder();
+	public static final String NEWLINE = System.lineSeparator();
+
+	@Override
+	public void run(String... args) throws InterruptedException {
+		log.info("EXECUTING : command line runner");
+
+		Thread pub = new Thread(this::publish);
+		pub.setName("PUBLISHER");
+
+		pub.start();
+	}
+
+	private void publish() {
+		Scanner session = new Scanner(System.in);
+		while (true) {
+			String message = session.nextLine();
+
+			if (message.equals("stop")) {
+				broker.add(message);
+				log.info(event.toString());
+				break;
+			}
+
+			event.append(Thread.currentThread().getName())
+					.append(" | Published -> ")
+					.append(message)
+					.append(NEWLINE);
+
+			broker.add(message);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				log.error(e.getMessage());
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
 }
