024863eb3976d6adea9acecbb5f069c3d6addd34
Yaroslav <GUSbYa@gmail.com>
2020-03-06 14:28:20 +0100

    initial commit

---
+++ publisher/src/test/java/lab/experimental/publisher/PublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.experimental.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
